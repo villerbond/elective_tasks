d1cc24047e910333808cf05228c0f2d14d1ce8a2
villerbond <91826226+villerbond@users.noreply.github.com>
2024-04-05 19:48:21 +0300

    Update task3-mpi.cpp

--- task3/MPI/task3-mpi.cpp
+++ task3/MPI/task3-mpi.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <chrono>
 
-// Пример функции f(x, y) = x^2 + y^2
+// РџСЂРёРјРµСЂ С„СѓРЅРєС†РёРё f(x, y) = x^2 + y^2
 double function(double x, double y) {
     return x * x + y * y;
 }
@@ -18,20 +18,20 @@ void print(std::vector<std::vector<double>> matrix, const int size) {
 }
 
 int main(int argc, char** argv) {
-    // Статус отправки/получения
+    // РЎС‚Р°С‚СѓСЃ РѕС‚РїСЂР°РІРєРё/РїРѕР»СѓС‡РµРЅРёСЏ
     MPI_Status status;
     MPI_Init(&argc, &argv);
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    const int size_of_matrix = 10000; // размер сетки
-    double h = 0.01; // шаг сетки
+    const int size_of_matrix = 10000; // СЂР°Р·РјРµСЂ СЃРµС‚РєРё
+    double h = 0.01; // С€Р°Рі СЃРµС‚РєРё
 
-    std::vector<std::vector<double>> A(size_of_matrix, std::vector<double>(size_of_matrix)); // исходный массив значений функции
-    std::vector<std::vector<double>> B(size_of_matrix, std::vector<double>(size_of_matrix)); // массив для производной
+    std::vector<std::vector<double>> A(size_of_matrix, std::vector<double>(size_of_matrix)); // РёСЃС…РѕРґРЅС‹Р№ РјР°СЃСЃРёРІ Р·РЅР°С‡РµРЅРёР№ С„СѓРЅРєС†РёРё
+    std::vector<std::vector<double>> B(size_of_matrix, std::vector<double>(size_of_matrix)); // РјР°СЃСЃРёРІ РґР»СЏ РїСЂРѕРёР·РІРѕРґРЅРѕР№
 
-    // Заполнение массива A
+    // Р—Р°РїРѕР»РЅРµРЅРёРµ РјР°СЃСЃРёРІР° A
     for (int i = 0; i < size_of_matrix; i++) {
         for (int j = 0; j < size_of_matrix; j++) {
             A[i][j] = function(i * h, j * h);
@@ -40,9 +40,9 @@ int main(int argc, char** argv) {
 
     //print(A, size_of_matrix);
 
-    // Главный процесс
+    // Р“Р»Р°РІРЅС‹Р№ РїСЂРѕС†РµСЃСЃ
     if (rank == 0) {
-        // Заполнение массива A
+        // Р—Р°РїРѕР»РЅРµРЅРёРµ РјР°СЃСЃРёРІР° A
         for (int i = 0; i < size_of_matrix; i++) {
             for (int j = 0; j < size_of_matrix; j++) {
                 A[i][j] = function(i * h, j * h);
@@ -51,10 +51,10 @@ int main(int argc, char** argv) {
         int i, index, n_elements_recieved;
         int elements_per_process = size_of_matrix / size;
 
-        // Замер времени начала выполнения
+        // Р—Р°РјРµСЂ РІСЂРµРјРµРЅРё РЅР°С‡Р°Р»Р° РІС‹РїРѕР»РЅРµРЅРёСЏ
         auto start_time = std::chrono::high_resolution_clock::now();
 
-        // Раздача задач
+        // Р Р°Р·РґР°С‡Р° Р·Р°РґР°С‡
         if (size > 1) {
             for (i = 1; i < size - 1; i++) {
                 int index = i * elements_per_process;
@@ -73,7 +73,7 @@ int main(int argc, char** argv) {
             MPI_Send(&index, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
 
-        // Считаем самую первую часть производной
+        // РЎС‡РёС‚Р°РµРј СЃР°РјСѓСЋ РїРµСЂРІСѓСЋ С‡Р°СЃС‚СЊ РїСЂРѕРёР·РІРѕРґРЅРѕР№
 
         for (int i = 0; i < elements_per_process; i++) {
             for (int j = 0; j < size_of_matrix; j++) {
@@ -84,19 +84,19 @@ int main(int argc, char** argv) {
                     B[i][j] = (A[i][j] - A[i - 1][j]) / h;
                 }
                 else {
-                    B[i][j] = (A[i + 1][j] - A[i - 1][j]) / (2 * h); // Центральная разностная схема для производной
+                    B[i][j] = (A[i + 1][j] - A[i - 1][j]) / (2 * h); // Р¦РµРЅС‚СЂР°Р»СЊРЅР°СЏ СЂР°Р·РЅРѕСЃС‚РЅР°СЏ СЃС…РµРјР° РґР»СЏ РїСЂРѕРёР·РІРѕРґРЅРѕР№
                 }
             }
         }
 
-        // Получаем результаты производных от других процессам
+        // РџРѕР»СѓС‡Р°РµРј СЂРµР·СѓР»СЊС‚Р°С‚С‹ РїСЂРѕРёР·РІРѕРґРЅС‹С… РѕС‚ РґСЂСѓРіРёС… РїСЂРѕС†РµСЃСЃР°Рј
         for (int i = 1; i < rank; i++) {
             MPI_Recv(&index, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
             MPI_Recv(&n_elements_recieved, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
             MPI_Recv(&B[index][0], n_elements_recieved * size_of_matrix, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, &status);
         }
 
-        // Замер окончания работы
+        // Р—Р°РјРµСЂ РѕРєРѕРЅС‡Р°РЅРёСЏ СЂР°Р±РѕС‚С‹
         auto end_time = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> elapsed = end_time - start_time;
 
@@ -104,7 +104,7 @@ int main(int argc, char** argv) {
         std::cout << " Time: " << (elapsed.count()) << " seconds" << std::endl;
 
     }
-    // Работа неосновных процессов
+    // Р Р°Р±РѕС‚Р° РЅРµРѕСЃРЅРѕРІРЅС‹С… РїСЂРѕС†РµСЃСЃРѕРІ
     else {
         int n_elements_recieved, index;
         MPI_Recv(&n_elements_recieved, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
@@ -114,7 +114,7 @@ int main(int argc, char** argv) {
             MPI_COMM_WORLD, &status);
         MPI_Recv(&index, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
-        // Вычисление производной для полученной части массива
+        // Р’С‹С‡РёСЃР»РµРЅРёРµ РїСЂРѕРёР·РІРѕРґРЅРѕР№ РґР»СЏ РїРѕР»СѓС‡РµРЅРЅРѕР№ С‡Р°СЃС‚Рё РјР°СЃСЃРёРІР°
 
         for (int i = 0; i < n_elements_recieved; i++) {
             for (int j = 0; j < size_of_matrix; j++) {
@@ -125,12 +125,12 @@ int main(int argc, char** argv) {
                     B[i][j] = (A[i][j] - A[i - 1][j]) / h;
                 }
                 else {
-                    B[i][j] = (A[i + 1][j] - A[i - 1][j]) / (2 * h); // Центральная разностная схема для производной
+                    B[i][j] = (A[i + 1][j] - A[i - 1][j]) / (2 * h); // Р¦РµРЅС‚СЂР°Р»СЊРЅР°СЏ СЂР°Р·РЅРѕСЃС‚РЅР°СЏ СЃС…РµРјР° РґР»СЏ РїСЂРѕРёР·РІРѕРґРЅРѕР№
                 }
             }
         }
 
-        // Отправка вычисленных производных в главный процесс
+        // РћС‚РїСЂР°РІРєР° РІС‹С‡РёСЃР»РµРЅРЅС‹С… РїСЂРѕРёР·РІРѕРґРЅС‹С… РІ РіР»Р°РІРЅС‹Р№ РїСЂРѕС†РµСЃСЃ
 
         MPI_Send(&index, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
         MPI_Send(&n_elements_recieved, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
